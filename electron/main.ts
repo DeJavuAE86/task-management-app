0a9c2cc409a8e47d6a36f78955703a3aabf6c401
root <root@LAPTOP-OA24CKAT.>
2024-11-12 17:11:07 +0800

    feat: add electron main process files

---
+++ electron/main.ts
@@ -0,0 +1,37 @@
+import * as path from 'path';
+
+import { app, BrowserWindow } from 'electron';
+import isDev from 'electron-is-dev';
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // 加载应用
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../out/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
